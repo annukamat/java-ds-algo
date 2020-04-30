a4656c32aaf73977cd5e4c6417df67bc15d94931
annukamat <43543343+annukamat@users.noreply.github.com>
2020-04-30 09:40:48 +0530

    Add files via upload

---
+++ mDimArrays.java
@@ -0,0 +1,50 @@
+package arrays;
+
+import java.util.Scanner;
+
+public class mDimArrays {
+
+	public static void main(String[] args) {
+//matrix addition
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("enter dimension");
+		int rows = sc.nextInt();
+		int cols = sc.nextInt();
+		
+		//initialisation of array;
+		int a[][] = new int[rows][cols];
+		int b[][] = new int[rows][cols];
+		
+		System.out.println("enter array a:");
+		for(int i = 0; i<rows; i++) {
+			for(int j = 0; j<cols; j++) {
+				a[i][j] = sc.nextInt();
+			}
+		}
+		
+		System.out.println("enter array b: ");
+		for(int i = 0; i<rows; i++) {
+			for(int j = 0; j<cols; j++) {
+				b[i][j] = sc.nextInt();
+			}
+		}
+		
+		int c[][] = new int[rows][cols];
+		
+		for(int i = 0; i<rows; i++) {
+			for(int j = 0; j<cols; j++) {
+				c[i][j] = a[i][j] + b[i][j];
+			}
+		}
+		
+		for(int i = 0; i<rows; i++) {
+			for(int j = 0; j<cols; j++) {
+				 System.out.print(c[i][j] + " ");	
+			}
+			System.out.println();
+		}
+		
+	}
+
+}
