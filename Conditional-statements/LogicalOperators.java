e6047fe6f62686dc1cb0ccbc8234efbee973508b
annukamat <annukamat21@gmail.com>
2020-05-01 13:31:58 +0530

    Cond Folder created

---
+++ Conditional-statements/LogicalOperators.java
@@ -0,0 +1,16 @@
+package conditional_statement;
+
+public class LogicalOperators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//three types -> and(&&) ; OR(||) ; NOT(!)
+//		bitwise AND(&) ; bitwise OR (|) ; diferrence is bitwise dono condition check karega
+ 
+		int grade = 10;
+		if(grade == 10 | grade == 12) {
+			System.out.println("you will give board exems!");
+		}
+	}
+
+}
