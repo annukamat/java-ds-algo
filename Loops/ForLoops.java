ebeacbf4fb0c1f56151ea18c0f42a0bd3eea11c3
annukamat <annukamat21@gmail.com>
2020-05-02 09:13:31 +0530

    3rd commit

---
+++ Loops/ForLoops.java
@@ -0,0 +1,15 @@
+package Loops;
+
+public class ForLoops {
+
+	public static void main(String[] args) {
+		//sum of first 50 no.s :
+int sum = 0;
+int n = 50;
+for(int i=1; i<=n; i++) {
+	sum = sum+i;
+}
+System.out.println(sum);
+	}
+
+}
---
+++ Loops/pattern4.java
@@ -0,0 +1,35 @@
+package Loops;
+
+import java.util.Scanner;
+
+public class pattern4 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+//		for(int i=1; i<=n; i++) {
+//			for(int j=1; j<=n-i; j++) {
+//				System.out.print("  ");
+//			}
+//			for(int k=1; k<=i; k++) {
+//				System.out.print("*  ");
+//			}
+//			System.out.println();
+//		}
+		
+		int num = 1;
+		for(int i=1; i<=n; i++) {
+			for(int j=1; j<=n-i; j++) {
+				System.out.print("  ");
+			}
+			for(int k=1; k<=i; k++) {
+				System.out.print(num++ +"  ");
+			}
+			System.out.println();
+		}
+		
+	}
+
+}
