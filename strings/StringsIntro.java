e90b447775d2ab5cd3532454f777f92d9f0917e4
annukamat <annukamat21@gmail.com>
2020-05-03 13:28:54 +0530

    strings

---
+++ strings/StringsIntro.java
@@ -0,0 +1,12 @@
+package strings;
+
+public class StringsIntro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String name = "annu";
+		System.out.println(name.charAt(3));
+	}
+
+}
