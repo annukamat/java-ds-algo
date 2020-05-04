d25a1dd1ff5a5813a8fdf1038f622318eec914bf
annukamat <annukamat21@gmail.com>
2020-05-04 20:42:47 +0530

    Encapsulation

---
+++ oops/packages/MainClass.java
@@ -0,0 +1,16 @@
+package oops.packages;
+
+import oops.packages.models.Student;
+import oops.packages.models.Teacher;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Student obj = new  Student("Tom");
+		Teacher teach = new Teacher("jerry");
+
+	}
+
+}
---
+++ oops/packages/models/Teacher.java
@@ -0,0 +1,15 @@
+package oops.packages.models;
+
+public class Teacher {
+
+	
+	public String name;
+	
+	public Teacher(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+}
