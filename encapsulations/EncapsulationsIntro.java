3e4c04f9a5a235fbcbdda0a7df2699f83acad270
annukamat <annukamat21@gmail.com>
2020-05-08 20:30:56 +0530

    encapsulation

---
+++ encapsulations/EncapsulationsIntro.java
@@ -0,0 +1,15 @@
+package encapsulations;
+
+public class EncapsulationsIntro {
+
+	public static void main(String[] args) {
+		
+		Student obj = new Student();
+		
+		obj.setAge(4);
+		System.out.println(obj.getAge());
+
+		
+		
+	}
+}
